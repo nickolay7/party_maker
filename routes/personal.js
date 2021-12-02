f7611d878d6e8b2c21e9962b375e2175423ec592
Maxim <maxcadet92@gmail.com>
2021-12-02 16:39:32 +0500

    fix event personal

--- routes/personal.js
+++ routes/personal.js
@@ -1,16 +1,19 @@
 const router = require('express').Router();
-const {Entry, User} = require("../db/models/");
+const { Event, User } = require('../db/models');
 
 router
   .route('/')
   .get(async (req, res) => {
-    const user = await User.findOne({where: {id: req.session.userid}})
-    res.render('userEditMain', {user})
+    const user = await User.findOne({include: Event, where: { id: req.session.userid } });
+    const memberEvents = user.dataValues.Events;
+    // const organizer = await Event.findAll({where: {organizer: req.session.userid}})
+console.log(memberEvents[0].dataValues.title);
+    res.render('userEditMain', { memberEvents });
   })
   .post(async (req, res) => {
     // const {параметры человека} = req.body
-    const user = await User.findOne({where: {id: req.session.userid}})
-    res.render('userEditMain', {user})
+    const user = await User.findOne({ where: { id: req.session.userid } });
+    res.render('userEditMain', { user });
   });
 
 module.exports = router;
