101605374e3054a8767678b2857192f0db43425b
Maxim <maxcadet92@gmail.com>
2021-12-03 16:08:11 +0500

    fix personal

--- routes/personal.js
+++ routes/personal.js
@@ -1,13 +1,13 @@
-const router = require("express").Router();
-const {Event, User} = require("../db/models");
+const router = require('express').Router();
+const { Entry, Event, User } = require('../db/models');
 
 router
-  .route("/")
+  .route('/')
   .get(async (req, res) => {
     let organizer;
     try {
       organizer = await Event.findAll({
-        where: {organizer: String(req.session.userid)},
+        where: { organizer: String(req.session.userid) },
       });
     } catch (e) {
       console.log(e.message);
@@ -15,7 +15,7 @@ router
 
     const member = await User.findOne({
       include: Event,
-      where: {id: req.session.userid},
+      where: { id: req.session.userid },
     });
 
     const memberEvents = member.dataValues.Events;
@@ -23,29 +23,35 @@ router
 
     if (organizer.length !== 0) {
       if (memberEvents.length !== 0) {
-        return res.render("userEditMain", {memberEvents, organizer});
+        return res.render('userEditMain', { memberEvents, organizer });
       }
-      return res.render("userEditMain", {organizer});
+      return res.render('userEditMain', { organizer });
     }
 
     if (memberEvents.length !== 0) {
       if (organizer.length !== 0) {
-        return res.render("userEditMain", {memberEvents, organizer});
+        return res.render('userEditMain', { memberEvents, organizer });
       }
-      return res.render("userEditMain", {memberEvents});
+      return res.render('userEditMain', { memberEvents });
     }
 
-    res.render("userEditMain");
+    res.render('userEditMain');
   })
   .post(async (req, res) => {
     // const {параметры человека} = req.body
-    const user = await User.findOne({where: {id: req.session.userid}});
-    res.render("userEditMain", {user});
+    const user = await User.findOne({ where: { id: req.session.userid } });
+    res.render('userEditMain', { user });
   });
 
-router.route("/del/:uniq").get(async (req, res) => {
+router.route('/del/:uniq').get(async (req, res) => {
   console.log(req.params.uniq);
-  await Event.destroy({where: {event_identifier: req.params.uniq}});
+  await Event.destroy({ where: { event_identifier: req.params.uniq } });
+  res.redirect('/personal');
+});
+router.route('/await/:uniq').get(async (req, res) => {
+  console.log(req.params.uniq);
+const event = await Event.findOne({ where: { event_identifier: req.params.uniq } });
+await Entry.destroy({where:{event_id: event.id}})
   res.redirect('/personal');
 });
 
